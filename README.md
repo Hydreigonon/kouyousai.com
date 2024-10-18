d1f7950ee8b0a75307d645b91c9c384b81131ff3
Hydreigonon <penneema321@gmail.com>
2024-10-18 11:06:24 +0900

    all delete

--- README.md
+++
@@ -1,5 +0,0 @@
-# kouyousai
-Formal web page is https://koyosai.com/. Do it my own way.
-
-Please go to this page to start.
-https://hydreigonon.github.io/kouyousai.com/kouyousai.html
--- js/menu.js
+++
@@ -1,7 +0,0 @@
-/*-----HTML内へ記述する場合は、<script>〜</script>で、囲いましょう*/
-$(function() {
-    $('.Toggle').click(function() {
-        $(this).toggleClass('active');
-        $('.menu').toggleClass('open');
-    });
-});
