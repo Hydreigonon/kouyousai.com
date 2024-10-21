93ed5b663de8d3912004347f5b07d4dd2f97c0d5
yuma-harada <yu2ma1da7ha0ra@gmail.com>
2024-10-21 10:53:46 +0900

    feat: jsonファイルのイベント一覧を表示する機能を追加

--- kouyousai/src/app/event/page.tsx
+++ kouyousai/src/app/event/page.tsx
@@ -1,10 +1,9 @@
 import { EventItem } from "./EventModal";
+import eventList from "./eventList.json";
 
 export default function Home() {
-  const title: string = "男女でニコイチ！";
-  const imagePath: string = "/event_images/nicoichi.png"
-  const time: string = "一日目　9:30-10:10";
-  const description: string = "高専で1番仲良しな男女2人組はどこだ！2人で協力して景品ゲット！！";
+
+  const list = eventList.events;
 
   return (
     <>
@@ -13,12 +12,22 @@ export default function Home() {
           イベント一覧
         </h1>
       </div>
-      <EventItem
-        title={title}
-        imagePath={imagePath}
-        time={time}
-        description={description}
-      />
+      {list.map(event => (
+        <>
+        {event.image ? 
+        <div key={event.title}>
+          <EventItem
+            title={event.title}
+            imagePath={"/event_images/" + event.image}
+            time={event.time}
+            description={event.description}
+            />
+        </div>
+        :
+        <></>
+        }
+        </>
+      ))}
     </>
   );
 }
