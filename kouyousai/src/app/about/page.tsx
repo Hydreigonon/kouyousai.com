fef68e4b27ceea32723a71a149910784290faa65
Hydreigonon <penneema321@gmail.com>
2024-10-23 09:32:50 +0900

    add: aboutページを追加

---
+++ kouyousai/src/app/about/page.tsx
@@ -0,0 +1,20 @@
+export default function About() {
+
+  return (
+    <div>
+      <h1 className="pageTitle">
+        こうよう祭について
+      </h1>
+
+      <h2 className="text-4xl py-8 text-center">
+        Meaning
+      </h2>
+      <p className="text-2xl tracking-wide leading-10 text-center px-20">
+        昔々、ある豊田高専生が考えた。<br/>
+        秋で木々がこうようするころ、<br/>
+        文化祭で気分をこうようさせに、<br/>
+        豊田高専にいこうよう！
+      </p>
+    </div>
+  );
+}
