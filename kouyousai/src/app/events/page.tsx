490fe02d61ecc0ea37ed9def894a173ef1465e7b
yuma-harada <yu2ma1da7ha0ra@gmail.com>
2024-10-24 08:39:41 +0900

    refac: イベントページのパスを変更

--- kouyousai/src/app/events/page.tsx
+++ kouyousai/src/app/events/page.tsx
@@ -1,5 +1,13 @@
 import { EventItem } from "./EventModal";
 import eventList from "./eventList.json";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export default function Home() {
 
@@ -12,6 +20,7 @@ export default function Home() {
           イベント一覧
         </h1>
       </div>
+      {/* <Table>要素</Table> */}
       {list.map(event => (
         <>
         <div key={event.title}>
