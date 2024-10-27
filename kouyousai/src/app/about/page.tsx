618ab2f61e321498d2edd05c93eae1c20136712f
Hydreigonon <penneema321@gmail.com>
2024-10-27 16:35:11 +0900

    feat: aboutページ完成

--- kouyousai/src/app/about/page.tsx
+++ kouyousai/src/app/about/page.tsx
@@ -1,20 +1,49 @@
+import Header from "../ui/header";
+import Footer from "../ui/footer";
+import Overview from "../ui/overview";
+
 export default function About() {
 
   return (
-    <div>
-      <h1 className="pageTitle">
-        こうよう祭について
-      </h1>
+    <body className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-grow">
+        <h1 className="pageTitle">
+          こうよう祭について
+        </h1>
 
-      <h2 className="text-4xl py-8 text-center">
-        Meaning
-      </h2>
-      <p className="text-2xl tracking-wide leading-10 text-center px-20">
-        昔々、ある豊田高専生が考えた。<br/>
-        秋で木々がこうようするころ、<br/>
-        文化祭で気分をこうようさせに、<br/>
-        豊田高専にいこうよう！
-      </p>
-    </div>
+        <Overview 
+          title="Meaning" 
+          description={"昔々、ある豊田高専生が考えた。\n秋で木々がこうようするころ、\n文化祭で気分をこうようさせに、\n豊田高専にいこうよう"}
+        />
+        <div className="h-10"></div>
+        <Overview 
+          title="MEICAnとは？" 
+          description={"こうよう祭マスコットキャラクターの「MEICAn」！\n名前は豊田高専五学科の英語名の頭文字から来ています！\n"}
+        />
+        <div className="h-10"></div>
+        <Overview 
+          title="Exhibitions" 
+          description={"こうよう祭では各学科やクラスなどによる展示披露を主な目的としています！\n日々高専で学んでいることやそれぞれの活動の成果を発揮した素晴らしい展示をご覧ください！"}
+        />
+        <div className="h-10"></div>
+        <Overview 
+          title="Events" 
+          description={"入場して右手のステージでは、全日程を通してイベントが行われています！\nゲストによるスペシャルステージも！"}
+        />
+        <div className="h-10"></div>
+        <Overview 
+          title="Booths" 
+          description={"部活動やクラブなどの模擬店も盛りだくさん！\nドリンクも買えちゃうよ！"}
+        />
+        <div className="h-10"></div>
+        <Overview 
+          title="Food trucks" 
+          description={"キッチンガーもいっぱい！\nおなかいっぱい楽しもう！"}
+        />
+        <div className="h-10"></div>
+      </main>
+      <Footer />
+    </body>
   );
 }
