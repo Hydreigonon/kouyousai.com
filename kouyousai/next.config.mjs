519ab0c30388d1fcc4af33e1141c21eca8e072b6
tamagosushi <mitachi.ts75@gmail.com>
2024-11-02 05:53:04 +0900

    [fix] nextjsのコンフィグを変更

--- kouyousai/next.config.mjs
+++ kouyousai/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
+  output: "export",
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/kouyousai.com' : '',
+  basePath: '/kouyousai.com',
 };
 
 export default nextConfig;
